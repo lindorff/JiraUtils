387f0386a4a8af5d1d874d0ed794f8e012c3c173
Paul Henrik <henrik.paul@lindorff.com>
2018-06-21 13:30:36 +0300

    Refined test so that it reflects real data better

--- tests/jira.spec.ts
+++ tests/jira.spec.ts
@@ -24,21 +24,21 @@ describe("Jira", () => {
 
     describe("getIssueStatusEvents", () => {
         it("should find a status item", () => {
-            exampleIssue.changelog.histories[0].items = [historyItem({ field: "status", from: "a", to: "b" })];
+            exampleIssue.changelog.histories[0].items = [historyItem({ field: "status", from: "0", to: "1" })];
 
             const statusEvents = getIssueStatusEvents(exampleIssue);
             expect(statusEvents).to.have.lengthOf(1);
             expect(statusEvents[0].items).to.have.lengthOf(1);
 
             const item = statusEvents[0].items[0];
-            expect(item.from).to.equal("a");
-            expect(item.to).to.equal("b");
+            expect(item.from).to.equal("0");
+            expect(item.to).to.equal("1");
         });
 
         it("should find many status items", () => {
             exampleIssue.changelog.histories[0].items = [
-                historyItem({ field: "status", from: "a", to: "b" }),
-                historyItem({ field: "status", from: "c", to: "d" })
+                historyItem({ field: "status", from: "0", to: "1" }),
+                historyItem({ field: "status", from: "2", to: "3" })
             ];
 
             const statusEvents = getIssueStatusEvents(exampleIssue);
@@ -46,16 +46,16 @@ describe("Jira", () => {
             expect(statusEvents[0].items).to.have.lengthOf(2);
 
             const item1 = statusEvents[0].items[0];
-            expect(item1.from).to.equal("a");
-            expect(item1.to).to.equal("b");
+            expect(item1.from).to.equal("0");
+            expect(item1.to).to.equal("1");
 
             const item2 = statusEvents[0].items[1];
-            expect(item2.from).to.equal("c");
-            expect(item2.to).to.equal("d");
+            expect(item2.from).to.equal("2");
+            expect(item2.to).to.equal("3");
         });
 
         it("should discard any non-status items", () => {
-            exampleIssue.changelog.histories[0].items = [historyItem({ field: "foo", from: "a", to: "b" })];
+            exampleIssue.changelog.histories[0].items = [historyItem({ field: "foo", from: "0", to: "1" })];
 
             const statusEvents = getIssueStatusEvents(exampleIssue);
             expect(statusEvents).to.have.lengthOf(0);
